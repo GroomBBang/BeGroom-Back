5adbde5c361a3838d491a76843c28d1c6364e743
Eunduk2 <dmsqls6789@naver.com>
2025-12-24 17:19:36 +0900

    project init

---
+++ src/main/java/com/example/BeGroom/BeGroomApplication.java
@@ -0,0 +1,13 @@
+package com.example.BeGroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeGroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeGroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BeGroom/BeGroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BeGroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeGroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
