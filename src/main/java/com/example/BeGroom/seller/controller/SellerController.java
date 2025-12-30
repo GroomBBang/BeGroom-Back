b07151f53e4d93f468640fe629422978e27038ba
zosunny <zoheyy108@naver.com>
2025-12-30 23:36:52 +0900

    feat: Seller 가입 기능 구현

--- src/main/java/com/example/BeGroom/seller/controller/SellerController.java
+++ src/main/java/com/example/BeGroom/seller/controller/SellerController.java
@@ -1,4 +1,45 @@
 package com.example.BeGroom.seller.controller;
 
+import com.example.BeGroom.common.response.CommonSuccessDto;
+import com.example.BeGroom.seller.domain.Seller;
+import com.example.BeGroom.seller.dto.SellerCreateReqDto;
+import com.example.BeGroom.seller.dto.SellerCreateResDto;
+import com.example.BeGroom.seller.service.SellerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/seller")
+@RequiredArgsConstructor
+@Tag(name = "Seller API", description = "판매자 관련 API")
 public class SellerController {
+
+    private final SellerService sellerService;
+
+    // API 1. 회원가입
+    @PostMapping("/join")
+    @Operation(summary = "회원가입", description = "새로운 판매자를 등록합니다.")
+    public ResponseEntity<CommonSuccessDto<SellerCreateResDto>> create(
+            @Valid @RequestBody SellerCreateReqDto sellerCreateReqDto
+    ){
+        Seller seller = sellerService.create(sellerCreateReqDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(
+                        CommonSuccessDto.of(
+                                new SellerCreateResDto(seller.getId()),
+                                HttpStatus.CREATED,
+                                "회원가입 성공"
+                        )
+                );
+    }
+
 }
--- src/main/java/com/example/BeGroom/seller/service/SellerServiceImpl.java
+++ src/main/java/com/example/BeGroom/seller/service/SellerServiceImpl.java
@@ -1,4 +1,36 @@
 package com.example.BeGroom.seller.service;
 
-public class SellerServiceImpl {
+import com.example.BeGroom.seller.domain.Seller;
+import com.example.BeGroom.seller.dto.SellerCreateReqDto;
+import com.example.BeGroom.seller.repository.SellerRepository;
+import jakarta.persistence.EntityExistsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SellerServiceImpl implements SellerService{
+
+    private final SellerRepository sellerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Seller create(SellerCreateReqDto sellerCreateReqDto) {
+        if(sellerRepository.findByEmail(sellerCreateReqDto.getEmail()).isPresent()) {
+            throw new EntityExistsException("이미 존재하는 판매자입니다.");
+        }
+
+        Seller seller = Seller.createSeller(
+                sellerCreateReqDto.getEmail(),
+                sellerCreateReqDto.getName(),
+                passwordEncoder.encode(sellerCreateReqDto.getPassword()),
+                sellerCreateReqDto.getPhoneNumber()
+        );
+
+        sellerRepository.save(seller);
+
+        return seller;
+    }
+
 }
