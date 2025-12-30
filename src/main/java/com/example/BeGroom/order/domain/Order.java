03498338056631ea84cd3d77bf3567c5949940d6
Eunduk2 <dmsqls6789@naver.com>
2025-12-30 17:45:51 +0900

    feat: Order 더미 엔티티 생성

---
+++ src/main/java/com/example/BeGroom/order/domain/Order.java
@@ -0,0 +1,18 @@
+package com.example.BeGroom.order.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+
+    @Id @GeneratedValue
+    private Long id;
+
+}
