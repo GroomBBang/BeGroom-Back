534d71d39e3495b965c9f1694d577a5cbff6973f
Park Jun Seok <112086285+YBSeok@users.noreply.github.com>
2026-01-02 13:33:01 +0900

    [Feat/noti] 로그인 응답 수정 및 SSE 설정 #21 (#27)
    * feat: add send all noti api
    
    * feat: add sse connection
    
    * feat: add fields in login res dto
    
    ---------
    
    Co-authored-by: 박준석 <parkjunseok@bagjunseog-ui-MacBookPro.local>

--- src/main/java/com/example/BeGroom/auth/controller/AuthController.java
+++ src/main/java/com/example/BeGroom/auth/controller/AuthController.java
@@ -8,6 +8,7 @@ import com.example.BeGroom.auth.service.AuthService;
 import com.example.BeGroom.common.response.CommonSuccessDto;
 import com.example.BeGroom.member.domain.Member;
 import com.example.BeGroom.member.domain.Role;
+import com.example.BeGroom.notification.repository.MemberNotificationRepository;
 import com.example.BeGroom.seller.domain.Seller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,6 +27,7 @@ public class AuthController {
 
     private final AuthService authService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final MemberNotificationRepository memberNotificationRepository;
 
     @PostMapping
     @Operation(summary = "로그인", description = "로그인에 성공하면 JWT 토큰을 반환한다.")
@@ -33,8 +35,19 @@ public class AuthController {
             @Valid @RequestBody MemberLoginReqDto reqDto
     ) {
         String jwtToken = "";
+        Long userId = 0L;
+        String name = "";
+        String email = "";
+        Long unreadNotisCount = 0L;
+        Role role = null;
+
         if(reqDto.getRole() == Role.SELLER){
             Seller seller = authService.sellerLogin(reqDto);
+            userId = seller.getId();
+            name = seller.getName();
+            email = seller.getEmail();
+            role = seller.getRole();
+
             jwtToken = jwtTokenProvider.createToken(
                     seller.getId(),
                     seller.getEmail(),
@@ -42,6 +55,11 @@ public class AuthController {
             );
         }else{
             Member member = authService.memberLogin(reqDto);
+            userId = member.getId();
+            name = member.getName();
+            email = member.getEmail();
+            role = member.getRole();
+
             jwtToken = jwtTokenProvider.createToken(
                     member.getId(),
                     member.getEmail(),
@@ -52,8 +70,8 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(
                         CommonSuccessDto.of(
-                                new MemberLoginResDto(jwtToken),
-                                HttpStatus.CREATED,
+                                new MemberLoginResDto(jwtToken, userId, name, email, memberNotificationRepository.countByMemberIdAndIsReadFalse(userId), role),
+                                HttpStatus.OK,
                                 "로그인 성공"
                         )
                 );
