505c2b204aa745e8a42658f17f5a4c6f11fb91cc
박준석 <parkjunseok@bagjunseog-ui-MacBookPro.local>
2026-01-02 13:29:50 +0900

    feat: add fields in login res dto

--- src/main/java/com/example/BeGroom/auth/dto/MemberLoginResDto.java
+++ src/main/java/com/example/BeGroom/auth/dto/MemberLoginResDto.java
@@ -1,5 +1,6 @@
 package com.example.BeGroom.auth.dto;
 
+import com.example.BeGroom.member.domain.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -15,4 +16,9 @@ public class MemberLoginResDto {
     @Schema(example = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJob25nQG5hdmVyLmNvbSIsInJvbGUiOiJVU0VSIiwiaWF0IjoxNzY2NzM5NjAzLCJleHAiOjE3NjY3OTk2MDN9.N4fVghRunMOYOHjTHPXnEPoPBvxymO0GPFvzd-ORot_ZZxGqaLKF1x2LIgeXdH8pwMqXlDqQI9_7A0w2FiV7pw")
     private String token;
 
+    private Long memberId;
+    private String name;
+    private String email;
+    private Long unreadNotisCount;
+    private Role role;
 }
