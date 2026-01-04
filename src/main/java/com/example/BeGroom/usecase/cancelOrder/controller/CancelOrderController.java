2d00829cb0395c128474d198f20a4e1e7da84ff8
EunDuk2 <dmsqls6789@naver.com>
2026-01-04 13:55:05 +0900

    feat: 주문 취소 기능 api 문서화

--- src/main/java/com/example/BeGroom/usecase/cancelOrder/controller/CancelOrderController.java
+++ src/main/java/com/example/BeGroom/usecase/cancelOrder/controller/CancelOrderController.java
@@ -4,6 +4,8 @@ import com.example.BeGroom.auth.domain.UserPrincipal;
 import com.example.BeGroom.common.response.CommonSuccessDto;
 import com.example.BeGroom.usecase.cancelOrder.dto.CancelOrderResDto;
 import com.example.BeGroom.usecase.cancelOrder.service.CancelOrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "CancelOrder API", description = "주문 취소 유스케이스 API")
 public class CancelOrderController {
 
     private final CancelOrderService cancelOrderService;
 
     @PostMapping("/orders/{orderId}/cancel")
+    @Operation(summary = "주문 취소", description = "결제완료된 주문을 취소한다.")
     public ResponseEntity<CommonSuccessDto<CancelOrderResDto>> cancelOrder(
             @AuthenticationPrincipal UserPrincipal user,
             @PathVariable Long orderId
