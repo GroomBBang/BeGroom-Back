b591b46c66f141dd2e5046f567ce1694d06fa787
Eunduk2 <dmsqls6789@naver.com>
2026-01-22 10:10:10 +0900

    test: OrderService - checkout 테스트 코드 작성

--- src/test/java/com/example/BeGroom/order/service/OrderServiceImplTest.java
+++ src/test/java/com/example/BeGroom/order/service/OrderServiceImplTest.java
@@ -9,8 +9,14 @@ import com.example.BeGroom.order.domain.OrderProduct;
 import com.example.BeGroom.order.domain.OrderStatus;
 import com.example.BeGroom.order.dto.OrderCreateReqDto;
 import com.example.BeGroom.order.dto.OrderProductReqDto;
+import com.example.BeGroom.order.dto.checkout.CheckoutResDto;
+import com.example.BeGroom.order.dto.checkout.CheckoutStatus;
 import com.example.BeGroom.order.repository.OrderProductRepository;
 import com.example.BeGroom.order.repository.OrderRepository;
+import com.example.BeGroom.payment.domain.Payment;
+import com.example.BeGroom.payment.domain.PaymentMethod;
+import com.example.BeGroom.payment.domain.PaymentStatus;
+import com.example.BeGroom.payment.repository.PaymentRepository;
 import com.example.BeGroom.product.domain.Brand;
 import com.example.BeGroom.product.domain.Product;
 import com.example.BeGroom.product.domain.ProductDetail;
@@ -19,14 +25,21 @@ import com.example.BeGroom.product.repository.ProductDetailRepository;
 import com.example.BeGroom.product.repository.ProductRepository;
 import com.example.BeGroom.seller.domain.Seller;
 import com.example.BeGroom.seller.repository.SellerRepository;
+import com.example.BeGroom.wallet.domain.Wallet;
+import com.example.BeGroom.wallet.repository.WalletRepository;
+import com.example.BeGroom.wallet.repository.WalletTransactionRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+import static com.example.BeGroom.order.dto.checkout.CheckoutStatus.COMPLETED;
+import static com.example.BeGroom.payment.domain.PaymentMethod.POINT;
+import static com.example.BeGroom.payment.domain.PaymentStatus.APPROVED;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -48,11 +61,20 @@ class OrderServiceImplTest extends IntegrationTestSupport {
     private OrderRepository orderRepository;
     @Autowired
     private OrderProductRepository orderProductRepository;
+    @Autowired
+    private WalletRepository walletRepository;
+    @Autowired
+    private WalletTransactionRepository walletTransactionRepository;
+    @Autowired
+    private PaymentRepository paymentRepository;
 
     @AfterEach
     void tearDown() {
+        paymentRepository.deleteAllInBatch();
         orderProductRepository.deleteAllInBatch();
         orderRepository.deleteAllInBatch();
+        walletTransactionRepository.deleteAllInBatch();
+        walletRepository.deleteAllInBatch();
         memberRepository.deleteAllInBatch();
         productDetailRepository.deleteAllInBatch();
         productRepository.deleteAllInBatch();
@@ -62,14 +84,102 @@ class OrderServiceImplTest extends IntegrationTestSupport {
 
 
     /* =========================
-     *  주문 생성 성공
+     *  주문 생성
      * ========================= */
 
+    @Nested
+    @DisplayName("주문 생성")
+    class CreateOrder {
+
+        @Test
+        @DisplayName("정상적인 요청이면 주문이 생성되고 저장된다")
+        void create_success() {
+            // given
+            Member member = createAndSaveMember();
+
+            Product product = createAndSaveProductHierarchy();
+
+            ProductDetail productDetail1 = createAndSaveProductDetail(product, 3000, 5);
+            ProductDetail productDetail2 = createAndSaveProductDetail(product, 5000, 5);
+
+            OrderCreateReqDto orderCreateReqDto =
+                    createOrderCreateReqDto(
+                            productDetail1, 1,
+                            productDetail2, 2
+                    );
+
+            // when
+            Order order = orderService.create(member.getId(), orderCreateReqDto);
+
+            // then
+            assertThat(order.getId()).isNotNull();
+            assertThat(order.getMember().getId()).isEqualTo(member.getId());
+            assertThat(order.getOrderStatus()).isEqualTo(OrderStatus.CREATED);
+            assertThat(order.getTotalAmount()).isEqualTo(13000);
+
+            assertThat(order.getOrderProductList()).hasSize(2)
+                    .extracting(
+                            op -> op.getProductDetail().getProductDetailId(),
+                            OrderProduct::getQuantity,
+                            OrderProduct::getPrice
+                    )
+                    .containsExactlyInAnyOrder(
+                            tuple(productDetail1.getProductDetailId(), 1, 3000),
+                            tuple(productDetail2.getProductDetailId(), 2, 5000)
+                    );
+
+            // 실제 저장 여부
+            assertThat(orderRepository.findById(order.getId())).isPresent();
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 회원이면 주문 생성에 실패한다")
+        void fail_when_member_not_found() {
+            // given
+            Product product = createAndSaveProductHierarchy();
+            ProductDetail productDetail = createAndSaveProductDetail(product, 3000, 5);
+
+            OrderCreateReqDto orderCreateReqDto =
+                    createOrderCreateReqDto(
+                            productDetail, 1,
+                            productDetail, 2
+                    );
+
+            Long invalidMemberId = 9999L;
+
+            // when then
+            assertThatThrownBy(() -> orderService.create(invalidMemberId, orderCreateReqDto))
+                    .isInstanceOf(EntityNotFoundException.class)
+                    .hasMessage("없는 사용자입니다.");
+        }
+
+        @Test
+        @DisplayName("존재하지 않는 상품 옵션이 포함되면 주문 생성에 실패한다")
+        void fail_when_product_detail_not_found() {
+            // given
+            Member member = createAndSaveMember();
+
+            OrderCreateReqDto orderCreateReqDto =
+                    new OrderCreateReqDto(
+                            List.of(
+                                    new OrderProductReqDto(9999L, 1)
+                            )
+                    );
+
+            // when then
+            assertThatThrownBy(() -> orderService.create(member.getId(), orderCreateReqDto))
+                    .isInstanceOf(EntityNotFoundException.class)
+                    .hasMessage("없는 상품 옵션입니다.");
+        }
+    }
+
+
+    @DisplayName("주문에 대해 체크아웃을 수행하면 결제가 승인되고 주문이 완료된다")
     @Test
-    @DisplayName("정상적인 요청이면 주문이 생성되고 저장된다")
-    void create_success() {
+    void checkout_success() {
         // given
         Member member = createAndSaveMember();
+        createAndSaveWallet(member);
 
         Product product = createAndSaveProductHierarchy();
 
@@ -82,82 +192,24 @@ class OrderServiceImplTest extends IntegrationTestSupport {
                         productDetail2, 2
                 );
 
-        // when
         Order order = orderService.create(member.getId(), orderCreateReqDto);
 
+        // when
+        CheckoutResDto resDto = orderService.checkout(member.getId(), order.getId(), POINT);
+
         // then
-        assertThat(order.getId()).isNotNull();
-        assertThat(order.getMember().getId()).isEqualTo(member.getId());
-        assertThat(order.getOrderStatus()).isEqualTo(OrderStatus.CREATED);
-        assertThat(order.getTotalAmount()).isEqualTo(13000);
+        assertThat(resDto.getOrderId()).isEqualTo(1L);
+        assertThat(resDto.getPaymentId()).isEqualTo(1L);
+        assertThat(resDto.getCheckoutStatus()).isEqualTo(COMPLETED);
 
-        assertThat(order.getOrderProductList()).hasSize(2)
-                .extracting(
-                        op -> op.getProductDetail().getProductDetailId(),
-                        OrderProduct::getQuantity,
-                        OrderProduct::getPrice
-                )
-                .containsExactlyInAnyOrder(
-                        tuple(productDetail1.getProductDetailId(), 1, 3000),
-                        tuple(productDetail2.getProductDetailId(), 2, 5000)
-                );
+        Payment payment = paymentRepository.findById(1L).get();
+        assertThat(payment.getPaymentStatus()).isEqualTo(APPROVED);
+        assertThat(payment.getAmount()).isEqualTo(13000);
 
-        // 실제 저장 여부 (Service 테스트에서 이 정도는 OK)
-        assertThat(orderRepository.findById(order.getId())).isPresent();
+        Order completedOrder = orderRepository.findById(1L).get();
+        assertThat(completedOrder.getOrderStatus()).isEqualTo(OrderStatus.COMPLETED);
+        assertThat(completedOrder.getTotalAmount()).isEqualTo(13000);
     }
-
-
-    /* =========================
-     *  회원 조회 검증
-     * ========================= */
-
-    @Test
-    @DisplayName("존재하지 않는 회원이면 주문 생성에 실패한다")
-    void fail_when_member_not_found() {
-        // given
-        Product product = createAndSaveProductHierarchy();
-
-        ProductDetail productDetail = createAndSaveProductDetail(product, 3000, 5);
-
-        OrderCreateReqDto orderCreateReqDto =
-                createOrderCreateReqDto(
-                        productDetail, 1,
-                        productDetail, 2
-                );
-
-        Long invalidMemberId = 9999L;
-
-        // when then
-        assertThatThrownBy(() -> orderService.create(invalidMemberId, orderCreateReqDto))
-                .isInstanceOf(EntityNotFoundException.class)
-                .hasMessage("없는 사용자입니다.");
-    }
-
-
-    /* =========================
-     *  상품 옵션 조회 검증
-     * ========================= */
-
-    @Test
-    @DisplayName("존재하지 않는 상품 옵션이 포함되면 주문 생성에 실패한다")
-    void fail_when_product_detail_not_found() {
-        // given
-        Member member = createAndSaveMember();
-
-        OrderCreateReqDto orderCreateReqDto =
-                new OrderCreateReqDto(
-                        List.of(
-                                new OrderProductReqDto(9999L, 1)
-                        )
-                );
-
-        // when then
-        assertThatThrownBy(() -> orderService.create(member.getId(), orderCreateReqDto))
-                .isInstanceOf(EntityNotFoundException.class)
-                .hasMessage("없는 상품 옵션입니다.");
-    }
-
-
 
 
     /* =========================
@@ -177,6 +229,15 @@ class OrderServiceImplTest extends IntegrationTestSupport {
                 Role.USER
         );
         return memberRepository.save(member);
+    }
+
+    /* =========================
+     *  Wallet
+     * ========================= */
+
+    private Wallet createAndSaveWallet(Member member) {
+        Wallet wallet = Wallet.create(member, 50000L);
+        return walletRepository.save(wallet);
     }
 
     /* =========================
